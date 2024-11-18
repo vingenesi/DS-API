92b1b8dc2b3840a25e03a9f8ade3a5cd6b68b5b2
R47431 <rafaelsora0@gmail.com>
2024-11-18 18:24:19 -0300

    ola mundo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const productRouter = require("./routes/product")
 
 const app = express();
 
@@ -20,19 +21,20 @@ app.use(
   })
 );
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use('/products', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
