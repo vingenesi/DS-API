74fc0a20019e8a2f13bcade33ac8ce4facfa2584
R47431 <rafaelsora0@gmail.com>
2024-11-19 19:19:45 -0300

    teste2

--- config/config.js
+++ config/config.js
@@ -15,11 +15,11 @@ module.exports = {
     logging: process.env.LOGGING_TEST
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 5432
+    username: process.env.DB_USER_DEPLOY,
+    password: process.env.DB_PASS_DEPLOY,
+    database: process.env.DB_NAME_DEPLOY,
+    host: process.env.DB_HOST_DEPLOY,
+    dialect: process.env.DB_DIALECT_DEPLOY,
+    port: process.env.DB_PORT_DEPLOY || 5432
   }
 };
