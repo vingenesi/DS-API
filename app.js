bd25d6a5624eec2e1efe0758f9166704c8e5c094
vingenesi <vinnyvvv04@gmail.com>
2024-11-19 19:47:48 -0300

    Alterações para integração

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true // permite o envio de cookies se necessário
   })
