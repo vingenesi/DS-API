0f558936c75f34728fff3d9b4b339ab351bb2fcd
R47431 <rafaelsora0@gmail.com>
2024-11-19 19:09:13 -0300

    teste2

--- config/config.js
+++ config/config.js
@@ -1,17 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    development:{
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT
-    },
-    test:{
-        dialect: process.env.DB_DIALECT_TEST,
-        storage: process.env.STORAGE_TEST,
-        logging: process.env.LOGGING_TEST
-    }
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  },
+  test: {
+    dialect: process.env.DB_DIALECT_TEST,
+    storage: process.env.STORAGE_TEST,
+    logging: process.env.LOGGING_TEST
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  },
+};
